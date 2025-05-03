e094d6c828b65a4550f5429e4ed5a0dc464a6c67
alvatiezo <148808102+alvatiezo@users.noreply.github.com>
2025-05-03 14:02:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -583,8 +583,15 @@ if not st.session_state.enviado:
 
                     
 else:
-    st.success("✅ ¡Formulario enviado correctamente!")
-    if st.button("Enviar otro formulario"):
+    st.markdown("""
+    <div style='background-color:#D6EFD1; padding: 20px; border-radius: 10px; border: 2px solid #51924B; text-align: center;'>
+        <h2 style='color: #2C517A;'>✅ ¡Gracias por completar la encuesta!</h2>
+        <p style='color: #2C517A;'>Tus respuestas han sido registradas exitosamente.</p>
+    </div>
+    """, unsafe_allow_html=True)
+
+    if st.button("📝 Enviar otra respuesta"):
         st.session_state.enviado = False
         st.experimental_rerun()
+
 
