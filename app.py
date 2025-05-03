dfe7dcdc9739814893d23756f8d75f590d413681
alvatiezo <148808102+alvatiezo@users.noreply.github.com>
2025-05-03 15:24:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.markdown("""
     html, body, .stApp {
         color-scheme: light !important;
         background-color: #2C517A !important;
-        color: #2C517A !important;
+        color: #FAFEF3 !important;
     }
 
     /* Títulos principales */
