62753162813b005840e06734d76ceda269dc54c0
alvatiezo <148808102+alvatiezo@users.noreply.github.com>
2025-05-03 13:04:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ st.markdown(
 
     /* Inputs personalizados */
     .stSelectbox > div, .stRadio > div, .stMultiSelect > div, .stTextArea > div {
-        background-color: ##2C517A;
+        background-color: #2C517A;
         border-radius: 10px;
         color: white;
         padding: 10px;
